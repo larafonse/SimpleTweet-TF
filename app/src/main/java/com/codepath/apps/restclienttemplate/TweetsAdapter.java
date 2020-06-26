c9582b6fbf18b64c6db28da25c8a1a23445eee33
*randrade8311* <randrade709@gmail.com>
2020-06-26 11:06:48 -0700

    User can view tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -49,6 +49,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
+    //clean all elements of the recycler
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    //add a list of items
+    public void addALL(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
     //Define a viewholder
     public class ViewHolder extends RecyclerView.ViewHolder{
 
